ab3a539dca7229b8f0e4d013bb777648eb50c8b5
acech <acechler@gmail.com>
2022-12-19 11:51:53 -0500

    Cleanup the main file of commented out code.

--- main.cpp
+++ main.cpp
@@ -3,34 +3,6 @@
 
 
 
-/// Original While Loop Code
-    //    int ball_x = 100;
-    //    int ball_y = 100;
-    //    int ball_speed_x = 5;
-    //    int ball_speed_y = 5;
-    //    int ball_radius = 15;
-    // while (WindowShouldClose() == false)
-    // {
-    //     BeginDrawing();
-    //     ClearBackground(BLACK);
-    //     ball_x += ball_speed_x;
-    //     ball_y += ball_speed_y;
-
-    //     if (ball_x + ball_radius >= screenWidth || ball_x - ball_radius <= 0)
-    //     {
-    //         ball_speed_x *= -1;
-    //     }
-
-    //     if (ball_y + ball_radius >= screenHeight || ball_y - ball_radius <= 0)
-    //     {
-    //         ball_speed_y *= -1;
-    //     }
-
-    //     DrawCircle(ball_x, ball_y, ball_radius, WHITE);
-    //     EndDrawing();
-    // }
-
-
 typedef struct Timer {
     double startTime;   // Start time (seconds)
     double lifeTime;    // Lifetime (seconds)
