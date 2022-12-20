d566f2f3c41db2ffc3ee24117247d70f6a2e6f64
acech <acechler@gmail.com>
2022-12-20 16:10:08 -0500

    Experimenting with a graph datastructure. Right now I am trying find away to pass the locations vector into drawGraph without data loss.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,53 @@
+#include <memory>
 #include "Examples\RotatingShapes.cpp"
 #include "Utilities\RaylibTimer.cpp"
 
+typedef struct{
+    int src, dest;
+} Edge;
+
+
+
+class Graph
+{
+
+public:
+    std::vector<std::vector<int>> adjacencyList;
+    Graph(std::vector<Edge> const &edges, int size) {
+        adjacencyList.resize(size);
+
+        for(auto &edge:edges){
+            adjacencyList[edge.src].push_back(edge.dest);
+            // uncomment the following code for undirected graph
+            // adjacencyList[edge.dest].push_back(edge.src);
+        }
+    }
+
+    std::vector<std::vector<int>> getList(){
+        return this->adjacencyList;
+    }
+
+    ~Graph() {}
+};
+
+void drawGraph(Graph const &graph, int size){
+
+    std::vector<Vector2> locations(size);
+
+    for(auto& location: locations){
+        location.x = GetRandomValue(1,GetScreenWidth());
+        location.y = GetRandomValue(1,GetScreenHeight());
+    }
+    for(int i = 0; i < size; ++i){
+        DrawRectangleV(locations[i],{32,32},WHITE);
+        for(int v: graph.adjacencyList[i]){
+            DrawLineV(locations[i],locations[v],GREEN);
+        }
+ 
+    }
+
+        
+}
 
 int main()
 {
@@ -8,7 +55,12 @@ int main()
     //RaylibTimer::test();
     const int screenWidth = 800;
     const int screenHeight = 600;
-
+    std::vector<Edge> edges = {
+        {0, 1}, {1, 2}, {2, 0}, {2, 1}, {3, 2}, {4, 5}, {5, 4}
+    };
+    int totalEdges = edges.size() - 1;
+    Graph graph(edges,totalEdges);
+    
     InitWindow(screenWidth, screenHeight, "HelloRaylib");
     SetTargetFPS(60);
 
@@ -16,7 +68,10 @@ int main()
 
         BeginDrawing();
         ClearBackground(BLACK);
-
+        
+        drawGraph(graph, totalEdges);
+        
+        
         EndDrawing();
     }
 
