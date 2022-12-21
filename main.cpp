cd087eaefc99fd21f1a80e9544df89cc851b6dbb
acech <acechler@gmail.com>
2022-12-21 11:58:20 -0500

    Cleaning the graph code.

--- main.cpp
+++ main.cpp
@@ -41,10 +41,19 @@ struct Vector2 rngVector(){
 void drawGraph(Graph const &graph, std::vector<Vector2> &locations, int size){
 
     const Vector2 RECT_SIZE{32,32};
+    int halfWidth = RECT_SIZE.x / 2;
+    int halfHeight = RECT_SIZE.y / 2;
     for(int i = 0; i < size; ++i){
         DrawRectangleV(locations.at(i), RECT_SIZE, WHITE);
+        DrawText(TextFormat("%01i", i), locations.at(i).x, locations.at(i).y, 20, GREEN);
         for(int v: graph.adjacencyList[i]){
-            DrawLineV(locations.at(i), locations.at(v), RED);
+            DrawCircle(locations.at(i).x + halfWidth, locations.at(i).y + halfHeight, 5.f, BLUE);
+            
+            DrawLine(locations.at(i).x + halfWidth, locations.at(i).y + halfHeight, 
+                     locations.at(v).x + halfWidth, locations.at(v).y + halfHeight,
+                     WHITE);
+
+            DrawCircle(locations.at(v).x + halfWidth, locations.at(v).y + halfHeight, 5.f, RED);
         }
     }
 }
@@ -58,12 +67,13 @@ int main()
     InitWindow(screenWidth, screenHeight, "HelloRaylib");
     SetTargetFPS(60);
     std::vector<Edge> edges = {
-        {0, 1}, {1, 2}, {2, 0}, {2, 1}, {3, 2}, {4, 5}, {5, 4}
+        {0, 1}, {0, 2}, {1, 2}, {2, 1}, {3, 2}, {3, 4}, {3,6}, {4, 5}, {5,6}
     };
     int totalEdges = edges.size() - 1;
     Graph graph(edges, totalEdges);
     std::vector<Vector2> locations(totalEdges);
     
+
     for (auto &location : locations){
         location = rngVector();
     }
